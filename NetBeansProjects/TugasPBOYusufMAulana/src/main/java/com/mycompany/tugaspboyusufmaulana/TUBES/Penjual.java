0ba748728aaa5c1b87747ea591201d42b4347b0f
UcupMelon15 <ucupmaulana151307@gmail.com>
2026-05-23 17:50:18 +0800

    first commit

---
+++ NetBeansProjects/TugasPBOYusufMAulana/src/main/java/com/mycompany/tugaspboyusufmaulana/TUBES/Penjual.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.tugaspboyusufmaulana.TUBES;
+
+/**
+ *
+ * @author ASUS
+ */
+public class Penjual {
+    
+    private String nama;
+    private int id;
+    
+    public Penjual(String nama, int id) {
+        this.nama = nama;
+        this.id = id;
+    }
+    
+}
